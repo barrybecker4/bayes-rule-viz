50c31508c1fab850167498cb879c68cd77a43d6a
Barry G. Becker <barrybecker4@yahoo.com>
2016-05-22 19:55:15 -0700

    Add sliders for params.

--- js/disease.js
+++ js/disease.js
@@ -3,10 +3,34 @@ var disease = (function(module) {
 
     module.init = function() {
 
+        $("#probability-diseased-slider").slider({
+            value: 0.1,
+            min: 0.01,
+            max: 1.0,
+            step: 0.01,
+            height: "10px",
+            slide: function(event, ui) {
+                $("#probability-diseased").text(ui.value);
+            }
+        });
+
+        $("#test-accuracy-slider").slider({
+            value: 99.0,
+            min: 98,
+            max: 100.0,
+            step: 0.1,
+            labels: true,
+            pips: true,
+            slide: function(event, ui) {
+                $("#test-accuracy").text(ui.value);
+            }
+        });
+
         var sankeyView = disease.sankeyView("#chart");
-        
+
+
     };
 
-    
+
     return module;
 } (disease || {}));
