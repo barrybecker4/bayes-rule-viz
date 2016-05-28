b5895091fd5e5cc1b5d069331130095ac5caee1c
Barry G. Becker <barrybecker4@yahoo.com>
2016-05-28 12:18:21 -0700

    Trying to add a formula. WIP.

--- js/bayesRuleView.js
+++ js/bayesRuleView.js
@@ -40,15 +40,16 @@ var disease = (function(module) {
                 "<mrow> <mn>2</mn><mi>a</mi> </mrow>" +
                 "</mfrac></mrow>" +
                 "</math>" +
-            "<div style='font-size: 150%;'>$$" +
-                "\definecolor{energy}{RGB}{114,0,172} " +
-                "\definecolor{freq}{RGB}{45,177,93} " +
-                "\definecolor{spin}{RGB}{251,0,29} " +
-                "\definecolor{signal}{RGB}{18,110,213} " +
-                "\definecolor{circle}{RGB}{217,86,16} " +
-                "\definecolor{average}{RGB}{203,23,206} " +
-                "\color{energy} X_{\color{freq} k} \color{black} = \color{average} \frac{1}{N} \sum_{n=0}^{N-1} " +
-                "\color{signal}x_n \color{spin}e^{\mathrm{i} \color{circle} 2\pi \color{freq}k \color{average} \frac{n}{N}}" +
+                "<div style='font-size: 150%;'>$$" +
+                "\\definecolor{energy}{RGB}{114,0,172} " +
+                "\\definecolor{freq}{RGB}{45,177,93} " +
+                "\\definecolor{spin}{RGB}{251,0,29} " +
+                "\\definecolor{signal}{RGB}{18,110,213} " +
+                "\\definecolor{circle}{RGB}{217,86,16} " +
+                "\\definecolor{average}{RGB}{203,23,206} " +
+                "\\color{energy} X_{\\color{freq} k} \\color{black} = " +
+                "\\color{average} \\frac{1}{N} \\sum_{n=0}^{N-1}" +
+                "\\color{signal}x_n \\color{spin}e^{\\mathrm{i} \\color{circle} 2\\pi \\color{freq}k \\color{average} \\frac{n}{N}}" +
             "$$" +
                 "</div>");
         };
