44d6d40e2b4bb0e4bb91fc3f5a14c9dc5c286bb1
Barry G. Becker <barrybecker4@yahoo.com>
2016-06-04 10:26:57 -0700

    Fix tooltip and highlighting.

--- js/vennDiagramView.js
+++ js/vennDiagramView.js
@@ -9,41 +9,29 @@ var disease = (function(module) {
 
         //var margin = {top: 10, right: 10, bottom: 10, left: 10};
         var TRANS_DURATION = 400;
+        var STROKE_WIDTH = 1;
+        var OVER_STROKE_WIDTH = 2;
+        var STROKE_OPACITY = 0.7;
+        var OVER_STROKE_OPACITY = 1.0;
+        var FILL_OPACITY = 0.3;
+        var OVER_FILL_OPACITY = 0.6;
 
         var my = {};
         var chart = venn.VennDiagram();
         var div;
         var tooltip;
+        var colors = [disease.TEST_NEG_HEALTHY, disease.POSITIVE_COLOR, disease.DISEASED_COLOR];
+        var cmap = d3.scale.ordinal();
 
         /** Add the initial svg structure */
         function init() {
             div = d3.select(parentEl);
 
+            cmap.domain(["Population", "Test positive", "Diseased"]).range(colors);
+
             // add a tooltip to place when mousing over
             tooltip = d3.select(parentEl).append("div")
                 .attr("class", "venntooltip");
-/*
-            svg.append("circle")
-                .attr("class", "population-circle")
-                .attr("opacity", 0.4).attr("fill", disease.TEST_NEG_HEALTHY)
-                .append("title").text("The whole population. Those outside the red circle are healthy.");
-            svg.append("circle")
-                .attr("class", "test-positive-circle")
-                .attr("opacity", 0.8).attr("fill", disease.POSITIVE_COLOR);
-            svg.append("circle")
-                .attr("class", "diseased-circle")
-                .attr("opacity", 0.2).attr("fill", disease.DISEASED_COLOR)
-                .append("title").text("These are healthy, but tested positive.");
-            svg.append("circle").on("click", function() {alert("hi")})
-                .attr("class", "diseased-circle")
-                .attr("opacity", 0.9).attr("fill", disease.TEST_NEG_DISEASED)
-                .style("mask", "url(#test-pos-mask)")
-                .append("title").text("These people have the disease, but they tested negative, so they will die.")
-            svg.append("circle")
-                .attr("class", "test-positive-circle")
-                .attr("opacity", 0.0)
-                .append("title").text("These people tested positive");
-                */
         }
 
         /** update the Venn diagram */
@@ -57,7 +45,19 @@ var disease = (function(module) {
             div.selectAll("path")
                 .style("stroke-opacity", 0)
                 .style("stroke", "#fff")
-                .style("stroke-width", 0);
+                .style("stroke-width", 0)
+
+            d3.selectAll(parentEl + " .venn-circle path")
+                .style("fill-opacity", FILL_OPACITY)
+                .style("fill", function(d, i) { return colors[i]; });
+                //.style("stroke-width", STROKE_WIDTH)
+                //.style("stroke-opacity", STROKE_OPACITY)
+                //.style("stroke", function(d, i) { return colors[i]; });
+
+            d3.selectAll(parentEl + " .venn-circle text")
+                .style("fill", "black")
+                .style("font-size", "16px")
+                .style("font-weight", "100");
 
             // add listeners to all the groups to display tooltip on mouseover
             var circles = div.selectAll("g");
@@ -67,15 +67,16 @@ var disease = (function(module) {
                     venn.sortAreas(div, d);
 
                     // Display a tooltip with the current size
-                    tooltip.transition().duration(TRANS_DURATION).style("opacity", .9);
+                    tooltip.transition().duration(TRANS_DURATION)
+                        .style("opacity", .7);
                     tooltip.text(d.size.toLocaleString() + " people");
 
                     // highlight the current path
                     var selection = d3.select(this).transition("tooltip").duration(TRANS_DURATION);
                     selection.select("path")
-                        .style("stroke-width", 2)
+                        .style("stroke-width", OVER_STROKE_WIDTH)
                         .style("fill-opacity", d.sets.length == 1 ? .4 : .1)
-                        .style("stroke-opacity", 1);
+                        .style("stroke-opacity", OVER_STROKE_OPACITY);
                 })
 
                 .on("mousemove", function() {
@@ -84,12 +85,15 @@ var disease = (function(module) {
                 })
 
                 .on("mouseout", function(d, i) {
+                    // remove the ttip
                     tooltip.transition().duration(TRANS_DURATION).style("opacity", 0);
+
+                    // restore circle styling
                     var selection = d3.select(this).transition("tooltip").duration(TRANS_DURATION);
                     selection.select("path")
                         .style("stroke-width", 0)
                         .style("fill-opacity", d.sets.length == 1 ? .25 : .0)
-                        .style("stroke-opacity", 0);
+                        .style("stroke-opacity", 0)
                 });
         };
 
