f60d5b29264327fdc09dcde9210ef0bbf1f498ec
Barry Becker <barrybecker4@gmail.com>
2019-05-11 07:54:33 -0700

    Pull out BayesRuleSimulation to separate component (#7)

--- js/App.vue.js
+++ js/App.vue.js
@@ -1,10 +1,12 @@
 import IntroductionContent from './IntroductionContent.vue.js'
+import BayesRuleSimulation from './BayesRuleSimulation.vue.js'
 import NotesContent from './NotesContent.vue.js'
 
 new Vue({
     el: "#app",
     components: {
         IntroductionContent,
+        BayesRuleSimulation,
         NotesContent,
     },
 
